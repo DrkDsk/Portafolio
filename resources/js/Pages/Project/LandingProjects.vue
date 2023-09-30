895acbe678508bf4f2e5e31d3f0061fa4407cfba
Alfredo Palacios <joseaph.1998@gmail.com>
2023-09-30 13:11:57 -0600

    cambio de gri

--- resources/js/Pages/Project/LandingProjects.vue
+++ resources/js/Pages/Project/LandingProjects.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
     <div id="projects" class="m-4 flex flex-col px-4">
         <p class="text-4xl text-sky-800 text-center mb-8">{{title}}</p>
-        <div class="grid grid-cols-2 w-11/12 mx-auto gap-6 px-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto gap-6 px-4">
             <ShowProject v-for="project in projects" v-bind:id="project.id" :project="project"></ShowProject>
         </div>
     </div>
--- resources/js/Pages/Technologies/LandingTechnologies.vue
+++ resources/js/Pages/Technologies/LandingTechnologies.vue
@@ -7,12 +7,12 @@ let props = defineProps({
 </script>
 
 <template>
-    <div class="flex flex-col w-10/12 mx-auto mb-8" id="skills">
+    <div class="m-4 flex flex-col px-4" id="skills">
         <div class="flex flex-row mx-auto justify-center my-4">
             <p class="text-sky-800 text-4xl">Habilidades y Tecnologías</p>
         </div>
 
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 w-11/12 mx-auto gap-6 px-4">
             <ShowTechnology v-for="technology in technologies" v-bind:id="technology.id" :technology="technology"></ShowTechnology>
         </div>
     </div>
