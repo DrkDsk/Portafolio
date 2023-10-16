4ddbf394c172c524c3f6e9c674f946a05a32ba58
Alfredo Palacios <joseaph.1998@gmail.com>
2023-10-16 13:21:40 -0600

    implementacion de swal

--- resources/js/Pages/Admin/Technologies/ShowAdminTechnology.vue
+++ resources/js/Pages/Admin/Technologies/ShowAdminTechnology.vue
@@ -14,17 +14,17 @@ const cover = props.technology?.imagePath ?? "/assets/img/default.jpg"
 </script>
 
 <template>
-    <Link :href="route('admin.technologies.edit', technology.id)" class="rounded-md bg-white shadow-md border px-4 py-6">
+    <div class="rounded-md bg-white shadow-md border px-4 py-6">
         <div class="flex flex-col">
-            <div>
-                <img loading="lazy" :src=cover alt="" class="h-96 w-full object-cover object-center">
-            </div>
+            <Link :href="route('admin.technologies.edit', technology.id)">
+                <img loading="lazy"  :src=cover alt="" class="h-96 w-full object-cover object-center">
+            </Link>
             <div class="gap-1 mt-3 flex flex-row justify-between">
                 <p class="text-black text-2xl font-bold capitalize">{{technology.name}}</p>
                 <DeleteButton :url="route('admin.technologies.destroy',technology.id)"></DeleteButton>
             </div>
         </div>
-    </Link>
+    </div>
 </template>
 
 <style scoped>
