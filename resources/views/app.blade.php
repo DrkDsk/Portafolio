382f1c8e0f67df3015f8aa8f7684846057bfb5ed
Alfredo Palacios <alfredopalacios@blue.gen.in>
2023-12-20 09:12:19 -0600

    titulo actualizado

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Mi portafolio</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Fonts -->
